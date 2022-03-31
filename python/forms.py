a6bf636be705576076496cd2564ab018ba376e25
Kleb <94545082+Spartan2909@users.noreply.github.com>
2022-03-31 08:37:11 +0100

    Update forms.py

--- python/forms.py
+++ python/forms.py
@@ -1,7 +1,7 @@
-from wtforms import Form, IntegerField, SelectMultipleField, SubmitField, validators
+from wtforms import Form, StringField, SelectMultipleField, SubmitField, validators
 
 class TicketForm(Form):
-    code = IntegerField('Please enter your ticket number', validators=[validators.DataRequired()])
+    code = StringField('Please enter your ticket number', validators=[validators.DataRequired()])
 
 class FoodForm(Form):
     foodChoices = SelectMultipleField('Choose your food', choices=options.options)
