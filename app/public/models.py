ec03db2b17eebf784bc12bd1ebd41e10bcb6f927
Kleb <94545082+Spartan2909@users.noreply.github.com>
2022-05-21 15:54:47 +0100

    begin implementing models

--- app/public/models.py
+++ app/public/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+class User(models.Model):
+    email = models.CharField(max_length=35)
+    pwdHash = models.CharField('password hash', max_length=64)
+
+class Ticket(models.Model):
+    ticketNum = models.CharField(max_length=30)
+    matchDate = models.DateField()
+    seatNum = models.CharField(max_length=6)
+    user = models.ForeignKey()
